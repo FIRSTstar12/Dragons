8e2b4864124afe0ee6ba5284eb8dfa70a4c4c9ea
FIRSTstar12 <143663224+FIRSTstar12@users.noreply.github.com>
2024-04-14 00:38:45 +0000

    added siver and lands

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ class user():
     self.foodTypes = foodTypes
 
 class dragon():
-  def __init__(self, speed = 75, color = "red", type = "Flying"):
+  def __init__(self, speed = 75, color = "", type = ""):
     self.speed = speed
     self.color = color
+    self.type = type
+class lands():
+  def __init__(self, name = "", food = "", type = ""):
+    self.name = name
     self.type = type
